29ed978ec2c523cb5ce60c21492d1e4d8f8e39e1
ZhouSusan <szhou089@gmail.com>
2022-11-20 20:11:23 -0800

    added test case to displayInfoTest() in MyInfoTest.java

--- src/test/java/JavaFun/MyInfoTest.java
+++ src/test/java/JavaFun/MyInfoTest.java
@@ -1,10 +1,12 @@
 package JavaFun;
-
+import JavaFun.MyInfo;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyInfoTest {
 
     @org.junit.jupiter.api.Test
-    void displayInfo() {
+    void displayInfoTest() {
+        MyInfo susan = new MyInfo("Susan Zhou", 33, "Chicago, IL");
+        assertEquals("My name is Susan Zhou.\nI am 33 years old.\nChicago, IL is my hometown.", susan.displayInfo());
     }
 }

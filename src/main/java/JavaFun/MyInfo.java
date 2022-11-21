f26f3278f56468789a3cb40857760a7e33de5a33
ZhouSusan <szhou089@gmail.com>
2022-11-20 20:06:38 -0800

    added displayInfo() and test case in main in MyInfo.java

--- src/main/java/JavaFun/MyInfo.java
+++ src/main/java/JavaFun/MyInfo.java
@@ -10,4 +10,13 @@ public class MyInfo {
         this._age = age;
         this._homeTown = homeTown;
     }
+
+    public String displayInfo() {
+        return "My name is " + this._name + ".\nI am " + this._age + " years old.\n" + this._homeTown + " is my hometown.";
+    }
+
+    public static void main(String[] args) {
+        MyInfo susan = new MyInfo("Susan Zhou", 33, "Chicago, IL");
+        System.out.println(susan.displayInfo());
+    }
 }

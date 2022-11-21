d7aca6adb9af092a4a49e850c3baf34599359682
ZhouSusan <szhou089@gmail.com>
2022-11-20 20:34:04 -0800

    added class TimeTest1 to measure execution time in TimeTest1.java

---
+++ src/main/java/JavaFun/TimeTest1.java
@@ -0,0 +1,15 @@
+package JavaFun;
+
+public class TimeTest1 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long tot = 0;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            tot += i;
+        }
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println(elapsedTime);
+    }
+
+}

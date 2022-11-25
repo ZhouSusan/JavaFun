1f3110dacc11d4ac0baa818620528cd4ca7e2faf
ZhouSusan <szhou089@gmail.com>
2022-11-24 20:14:06 -0800

    added addCustomer() and printAllCustomers() in CafeUtil.java

--- src/main/java/Cafe_Business_Logic/CafeUtil.java
+++ src/main/java/Cafe_Business_Logic/CafeUtil.java
@@ -22,8 +22,24 @@ public class CafeUtil {
 
     public void displayMenu(ArrayList<String> menuItems) {
         for (int l = 0; l < menuItems.size(); l++) {
-            System.out.format("%s %s", l, menuItems.get(l));
+            System.out.format("%s %s\n", l, menuItems.get(l));
         }
     }
-    
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name: ");
+
+        //The readLine() method of Console class in Java is used to read a single line of text from the console.
+        String userName = System.console().readLine();
+        customers.add(userName);
+
+        int count = customers.size()-1;
+        System.out.format("Hello, %s! There are %s customers ahead of you.\n", userName, count);
+    }
+
+    public void printAllCustomers(ArrayList<String> customers) {
+        for (int t = 0; t < customers.size(); t++) {
+            System.out.println(customers.get(t));
+        }
+    }
 }
